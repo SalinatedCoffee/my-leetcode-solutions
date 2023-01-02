a9883db78c2dfd61f98a1b905313d3043951bdf6
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-01-02 17:20:21 +0900

    Revert "Fix vim-induced indent oddities"
    This reverts commit 88a75faa934aa63538cb145a4fd0bddb920eff2b.

--- Python3/797. (M) All Paths From Source to Target/solution.py
+++ Python3/797. (M) All Paths From Source to Target/solution.py
@@ -1,33 +1,33 @@
 class Solution:
-  def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-    # naively traverse all items in graph
-    # guaranteed to work because graph is acyclic
-    # B/DFS both work, will be using DFS here
-    # Push tuples ([path], cur) to stack,
-    # append [path]+cur to result list when destination is reached
+    def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+				# naively traverse all items in graph
+				# guaranteed to work because graph is acyclic
+				# B/DFS both work, will be using DFS here
+				# Push tuples ([path], cur) to stack,
+				# append [path]+cur to result list when destination is reached
 
-    # DFS stack
-    stack = []
-    # List of correct paths
-    ret = []
-    # Destination node is n-1
-    dest = len(graph)-1
+				# DFS stack
+        stack = []
+				# List of correct paths
+        ret = []
+				# Destination node is n-1
+        dest = len(graph)-1
 
-    # Set up source node
-    for i in graph[0]:
-      stack.append(([0], i))
+				# Set up source node
+        for i in graph[0]:
+            stack.append(([0], i))
 
-    # Traverse
-    while len(stack):
-      path, cur = stack.pop()
-      path.append(cur)
-      # If destination node is reached
-      if cur == dest:
-        ret.append(path)
-        continue
-      # Push reachable neighbors to stack
-      for i in graph[cur]:
-        stack.append((path[:], i))
+				# Traverse
+        while len(stack):
+            path, cur = stack.pop()
+            path.append(cur)
+						# If destination node is reached
+            if cur == dest:
+                ret.append(path)
+                continue
+						# Push reachable neighbors to stack
+            for i in graph[cur]:
+                stack.append((path[:], i))
     
-    return ret
+        return ret
             
