077b9b0d241e6488d7528f2ed1155198d63c7313
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-01-10 16:59:09 +0900

    Add problem #100

---
+++ Python3/100. (E) Same Tree/solution.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    stack_p = [p]
+    stack_q = [q]
+    while stack_p:
+      cur_p = stack_p.pop()
+      cur_q = stack_q.pop()
+      if cur_p is None and cur_q is None:
+        continue
+      elif cur_p is None or cur_q is None:
+        return False
+      else:
+        if cur_p.val != cur_q.val:
+          return False
+        stack_p.append(cur_p.right)
+        stack_p.append(cur_p.left)
+        stack_q.append(cur_q.right)
+        stack_q.append(cur_q.left)
+    return True
+
