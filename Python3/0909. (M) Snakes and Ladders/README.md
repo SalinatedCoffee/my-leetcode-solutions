5fa7a1b14785f957b94828e4d271bf64c1ddedea
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-01-25 17:18:30 +0900

    Complete problem #909

--- Python3/0909. (M) Snakes and Ladders/README.md
+++ Python3/0909. (M) Snakes and Ladders/README.md
@@ -1,9 +1,11 @@
-## \<Problem Number\>. (\<Difficulty\>) \<Problem Title\>
+## 909. (M) Snakes and Ladders
 
-### `\<solution filename\>`
-\<Content\>  
+### `solution.py`
+The first thing that instinctively comes to mind (at least for me) is to solve this with backtracking. However we quickly realize that at a certain square it isn't clear when or when not to backtrack. Considering other approaches, we can see that BFS can be effective since we are asked to return the *least* number of moves that can be made to reach the last square.  
+This is one of those problems where coming up with a conceptual solution is easy but where the execution of implementing it into code is difficult. We first have to deal with the cell labels, which is done by counting the number of rows bottom-first and deciding whether to reverse the column number based on which row an element is in. We may now 'flatten' `board` into a 1D list for easy access to the elements of `board` via labels instead of indicies. Implementing the BFS traversal is rather trivial, where at a certain square we enqueue the 'descendant' squares (reachable squares from current with 1 more move) along with the number of moves taken to reach that square and repeat until the queue is empty. We may return early whenever we reach the last square since BFS guarantees that the shortest path to the destination is returned.  
+One more thing to note is that cycles may be present depending on how the snakes and ladders are positioned, and so we also keep a `set` to keep track of visited nodes.
 
 #### Conclusion
-\<Content\>  
+This solution runs in $O(n^2)$ time since there are $n^2$ squares total, and we do a fixed number (6 maximum) of operations on each of them. It also uses $O(6^{n^2})$ space since 6 elements are added to `seen` for each square. Further optimizations may be made by checking whether a square has been visited *before* queuing it in `seen`, or by using a list of length $n^2$ instead to keep track of visited nodes.  
   
 
--- Python3/0909. (M) Snakes and Ladders/solution.py
+++ Python3/0909. (M) Snakes and Ladders/solution.py
@@ -1,50 +1,45 @@
-from typing import List
-from collections import deque
-
 class Solution:
   def snakesAndLadders(self, board: List[List[int]]) -> int:
-    # instinct is to use backtracking, but there is no criteria on when to backtrack
+    # instinct is to use backtracking, but we don't know when to backtrack
     # least number of moves, use BFS instead
-    # how to handle cycles?
     self.n = len(board)
     self.last = self.n ** 2
 
+    # translate square to indices in board
     def get_square(square):
       square -= 1
       row = self.n - square // self.n - 1
       col = square % self.n
       if (square // self.n) % 2:
         col = self.n - col - 1
-      print(square+1, row, col)
 
       return board[row][col]
-
-    for i in range(1, self.last+1):
-      get_square(i)
-    #return 0
     
+    # 'flatten' board into 1D list
+    board_f = [-1]
+    for i in range(1, self.last+1):
+      board_f.append(get_square(i))
+   
     visit = deque()
     visit.append((1,0))
-    ret = []
+    seen = set()
     while len(visit):
       cur, moves = visit.popleft()
-      print(f'At sq. {cur} with {moves} moves')
+      # reached destination
       if cur == self.last:
-        ret.append(moves)
+        return moves
+      # check if already visited
+      if cur in seen:
         continue
-      if cur > self.last:
-        continue
-      val = get_square(cur)
-      if val != -1:
-        print(f'snake/ladder, move to {val}')
-        cur = val
-      for i in range(1, 7):
-        visit.append((cur+i,moves+1))
+      else:
+        seen.add(cur)
+      for i in range(cur+1, min(cur+6, self.last)+1):
+        # check if next has snake or ladder
+        if board_f[i] != -1:
+          nxt = board_f[i]
+        else:
+          nxt = i
+        visit.append((nxt,moves+1))
 
-    return min(ret) if ret else -1
+    return -1
 
-obj = Solution()
-b1 = [[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,-1,-1,-1,-1,-1],[-1,35,-1,-1,13,-1],[-1,-1,-1,-1,-1,-1],[-1,15,-1,-1,-1,-1]]
-b2 = [[-1,-1,-1],[-1,9,8],[-1,8,9]]
-ret = obj.snakesAndLadders(b2)
-print(ret)
