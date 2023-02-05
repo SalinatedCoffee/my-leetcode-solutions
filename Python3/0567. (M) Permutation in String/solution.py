96a21645013a3136b3487410745501cec9d0d3a5
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-02-05 14:44:38 +0900

    Add comments

--- Python3/0567. (M) Permutation in String/solution.py
+++ Python3/0567. (M) Permutation in String/solution.py
@@ -8,6 +8,7 @@ class Solution:
     if len(s1) > len(s2):
       return False
 
+    # count letters for s1 and s2[0:len(s1)]
     cnt1 = [0] * 26
     cnt2 = [0] * 26
     for i in range(len(s1)):
@@ -16,6 +17,7 @@ class Solution:
     
     i, j = 0, len(s1)
     while j < len(s2):
+      # check whether letter counts match
       match = True
       for a, b in zip(cnt1, cnt2):
         if a != b:
@@ -23,11 +25,13 @@ class Solution:
           break
       if match:
         return True
+      # advance window by 1
       cnt2[ord(s2[i])-97] -= 1
       cnt2[ord(s2[j])-97] += 1
       i += 1
       j += 1
 
+    # check letter count match for s2[-len(s1):]
     match = True
     for a, b in zip(cnt1, cnt2):
       if a != b:
