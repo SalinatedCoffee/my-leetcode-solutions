61eeb6904b6def093335d29f5b26a00e078a3b81
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-02-05 17:26:44 +0900

    Finish problem #2421

--- Python3/2421. (H) Number of Good Paths/solution.py
+++ Python3/2421. (H) Number of Good Paths/solution.py
@@ -1,52 +1,75 @@
 class Solution:
-  def numberOfGoodPaths(self, vals: list[int], edges: list[list[int]]) -> int:
-    self.count = 0
-    adj = [[] for _ in range(len(vals))]
+  def numberOfGoodPaths(self, vals: List[int], edges: List[List[int]]) -> int:
+    # union find approach
+    # number of paths can be computed without traversal
+    # incrementally build up subgraph while accumulating path count
+
+    n = len(vals)
+    # generate adjacency list: O(n)
+    adj = [[] for _ in range(n)]
     for u, v in edges:
       adj[u].append(v)
       adj[v].append(u)
-
-    #DFS based traversal
-    #give union find up to current when descending
-    #can reuse same object
-    #at parent node if repr node == self, count
-    #set union with self(singleton) and passed
-    p = [x for x in range(len(vals))]
-    def ufind(a):
-      pa = p[a]
-      if pa != a:
-        pa = ufind(pa)
-      return pa
     
-    def uunion(a, b):
-      pa, pb = ufind(a), ufind(b)
-      if vals[pa] > vals[pb]:
-        p[b] = pa
-      elif pb > pa:
-        p[a] = pb
-
-    def dfs(root, prev):
-      print(p, root, prev)
-      self.count += 1
-      if prev:
-        pprev = ufind(prev)
-        print(vals[pprev], vals[root], pprev, root)
-        if vals[pprev] == vals[root]:
-          self.count += 1
-        uunion(root, prev)
-      for i in adj[root]:
-        if i == prev:
-          continue
-        dfs(i, root)
-    print(adj)
-    dfs(0, None)
-
-    return self.count
-
-obj = Solution()
-v1 = [2,2,5,5]
-e1 = [[1,0],[0,2],[3,2]]
-v2 = [1,3,2,1,3]
-e2 = [[0,1],[0,2],[2,3],[2,4]]
-print(obj.numberOfGoodPaths(v1, e1))
+    # create sorted dict that maps values to set of nodes: O(nlogn)
+    vals_sorted = sorted(set(vals))
+    values_to_nodes = {}
+    for k in vals_sorted:
+      values_to_nodes[k] = []
+    for i in range(n):
+      values_to_nodes[vals[i]].append(i)
+    
+    # union find object that supports find and merge operations
+    class UFind:
+      def __init__(self, size):
+        self.parent = [x for x in range(size)]
+        # arbitrary rank as tiebreaker
+        self.rank = [0] * size
+      
+      # O(1)
+      def find(self, x):
+        if self.parent[x] != x:
+          self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+      
+      # O(1)
+      def union_set(self, x, y):
+        set_x, set_y = self.find(x), self.find(y)
+        # x and y already in same set (subgraph)
+        if set_x == set_y:
+          return
+        # merge according to rank
+        elif self.rank[set_x] < self.rank[set_y]:
+          self.parent[set_x] = set_y
+        elif self.rank[set_x] > self.rank[set_y]:
+          self.parent[set_y] = set_x
+        else:
+          # arbitrarily decide rank
+          self.parent[set_y] = set_x
+          self.rank[set_x] += 1
+    
+    obj = UFind(n)
+    good_paths = 0
+    # iterate over sets of nodes with same vals in ascending order: O(n)
+    for val, nodes in enumerate(values_to_nodes.values()):
+      for node in nodes:
+        for n in adj[node]:
+          # if neighbor has smaller value, add to node's subgraph
+          if vals[node] >= vals[n]:
+            obj.union_set(node, n)
+      # count size of all current subgraphs: O(n)
+      group = {}
+      for node in nodes:
+        # get unique id of subgraph where node is a member
+        node_g = obj.find(node)
+        # increment subgraph size
+        if node_g in group:
+          group[node_g] += 1
+        else:
+          group[node_g] = 1
+      # compute number of good paths for all subgraphs: O(n)
+      for c in group.values():
+        good_paths += (c*(c+1)//2)
+    
+    return good_paths
 
