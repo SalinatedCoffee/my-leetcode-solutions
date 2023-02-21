dfbf08e3023534ff87f3f09164af9d9f456a52e3
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-02-21 20:18:10 +0900

    Use copied grid instead of original

--- Problems/0200. (M) Number of Islands/Python3/solution_2.py
+++ Problems/0200. (M) Number of Islands/Python3/solution_2.py
@@ -7,17 +7,17 @@ class Solution:
 
     # recursively fill adjacent 1s with 0s
     def fill(m, n):
-      if 0 <= m < len(grid) and 0 <= n < len(grid[0]) and grid[m][n] == '1':
-        grid[m][n] = '0'
+      if 0 <= m < len(visited) and 0 <= n < len(visited[0]) and visited[m][n] == '1':
+        visited[m][n] = '0'
         next_coords = [(m-1,n),(m,n-1),(m+1,n),(m,n+1)]
         for n_m, n_n in next_coords:
           fill(n_m,n_n)
 
     islands = 0
     # iterate across grid, call fill() if current square is land
-    for i in range(len(grid)):
-      for j in range(len(grid[0])):
-        if grid[i][j] == '1':
+    for i in range(len(visited)):
+      for j in range(len(visited[0])):
+        if visited[i][j] == '1':
           islands += 1
           fill(i, j)
     
