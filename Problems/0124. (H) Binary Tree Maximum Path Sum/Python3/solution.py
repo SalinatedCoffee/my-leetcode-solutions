f93cb53c94ab929369099515be25c4c765a88123
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-02-27 18:10:19 +0900

    Fix typos

--- Problems/0124. (H) Binary Tree Maximum Path Sum/Python3/solution.py
+++ Problems/0124. (H) Binary Tree Maximum Path Sum/Python3/solution.py
@@ -9,7 +9,7 @@ class Solution:
       # recursively get left and right path sums
       left_sum = recurse(node.left) if node.left else 0
       right_sum = recurse(node.right) if node.right else 0
-      # see whether path in subtree rooted at node is longer than previous max
+      # see whether path sum in subtree rooted at node is larger than previous max
       self.max_sum = max(self.max_sum, left_sum+node.val+right_sum)
       ret = max(node.val+left_sum, node.val+right_sum)
       return ret if ret > 0 else 0
