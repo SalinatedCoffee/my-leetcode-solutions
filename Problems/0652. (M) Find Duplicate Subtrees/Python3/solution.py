69d32a37fd0cad55545370b20ce5a06f120cc1cd
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-02-28 15:45:44 +0900

    Change variable name to match code behavior

--- Problems/0652. (M) Find Duplicate Subtrees/Python3/solution.py
+++ Problems/0652. (M) Find Duplicate Subtrees/Python3/solution.py
@@ -9,14 +9,14 @@ class Solution:
       # get string representations of subtrees recursively
       l_str = recurse(node.left) if node.left else 'n'
       r_str = recurse(node.right) if node.right else 'n'
-      pre_str = l_str + str(node.val) + r_str + '.'
+      in_str = l_str + str(node.val) + r_str + '.'
       # no previous copy of tree rooted at node
-      if pre_str not in self.seen:
-        self.seen.add(pre_str)
+      if in_str not in self.seen:
+        self.seen.add(in_str)
       # duplicate not yet added to result pool
-      elif pre_str not in self.ret:
-        self.ret[pre_str] = node
-      return pre_str
+      elif in_str not in self.ret:
+        self.ret[in_str] = node
+      return in_str
 
     recurse(root)
 
