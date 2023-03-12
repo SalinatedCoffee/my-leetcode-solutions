401da2a603b567e51531acd1969b285c93c08c05
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-03-12 19:30:12 +0900

    Start problems #23 and #371

---
+++ Problems/0371. (M) Sum of Two Integers/Python3/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+  def getSum(self, a: int, b: int) -> int:
+    # bit manipulation
+    mask = 0xFFFFFFFF
+    
+    # half adder logic
+    while b:
+        a, b = (a ^ b) & mask, ((a & b) << 1) & mask
+    
+    # two's complement, flip sign as necessary
+    return a if a <= 0x7FFFFFFF else ~(a ^ mask)
+
