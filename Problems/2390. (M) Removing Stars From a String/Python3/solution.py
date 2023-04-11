a1a70a7d6d9f0f5390276cb340ee6c9aba45dea7
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-04-11 14:32:48 +0900

    Add problem #2390

---
+++ Problems/2390. (M) Removing Stars From a String/Python3/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+  def removeStars(self, s: str) -> str:
+    # stack
+    
+    stack = []
+    for c in s:
+      if c != '*':
+        stack.append(c)
+      else:
+        stack.pop()
+    
+    return ''.join(stack)
+
