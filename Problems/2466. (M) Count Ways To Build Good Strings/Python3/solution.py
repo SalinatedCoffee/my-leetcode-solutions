c8de34bd95cca029b6b347588ef42194bcbbbb11
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-05-22 17:14:08 +0900

    Complete problem #2466

---
+++ Problems/2466. (M) Count Ways To Build Good Strings/Python3/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+  def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+    # bottom-up dynamic programming (tabulation)
+
+    dp = [0] * (high + 1)
+    dp[0] = 1
+    mod = 10**9 + 7
+
+    for i in range(1, high+1):
+      if i >= zero:
+        dp[i] += dp[i-zero]
+      if i >= one:
+        dp[i] += dp[i-one]
+      dp[i] %= mod
+    
+    return sum(dp[low:high+1]) % mod
+
