07d5e59f5e6b52435363a65430d892130b65c64f
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-06-20 15:51:02 +0900

    Start problem #2090

---
+++ Problems/2090. (M) K Radius Subarray Averages/Python3/README.md
@@ -0,0 +1,13 @@
+## 2090. (M) K Radius Subarray Averages
+
+### `solution.py`
+
+  
+
+
+
+#### Conclusion
+
+\<Content\>  
+
+
---
+++ Problems/2090. (M) K Radius Subarray Averages/Python3/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+  def getAverages(self, nums: List[int], k: int) -> List[int]:
+    # prefix sums
+
+    n = len(nums)
+    p_sums = [0] * (n+1)
+    c_sum = 0
+    for i in range(n):
+      c_sum += nums[i]
+      p_sums[i+1] = c_sum
+
+    ret = [-1] * n
+    for i in range(k, n-k):
+      c_sum = p_sums[i+k+1] - p_sums[i-k] if k else nums[i]
+      ret[i] = c_sum // (k*2+1)
+    
+    return ret
+
