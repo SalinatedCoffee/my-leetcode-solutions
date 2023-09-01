3818b028c6cfd74651c7a8c7777ee2c7b8bdf8f0
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-09-01 14:56:01 +0900

    Make changes to writeup and preallocate return list instead of using .append()

--- Problems/0338. (E) Counting Bits/Python3/solution.py
+++ Problems/0338. (E) Counting Bits/Python3/solution.py
@@ -2,9 +2,9 @@ class Solution:
   def countBits(self, n: int) -> List[int]:
     # memoize previous results
     
-    ret = []
+    ret = [0] * (n+1)
     for i in range(n+1):
-      ret.append(ret[i//2] + i%2 if i else 0)
+      ret[i] = ret[i//2] + i%2 if i else 0
     
     return ret
 
