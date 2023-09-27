a86794ad8a31bf9b5f6b4a82947468ed064cbb0f
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2023-09-27 17:22:07 +0900

    Start problem 880

---
+++ Problems/0880. (M) Decoded String at Index/Python3/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+  def decodeAtIndex(self, s: str, k: int) -> str:
+    # reverse traversal
+
+    n = len(s)
+    tape_len, idx = 0, 0
+    # only consider substring that would include k
+    while tape_len < k:
+      if s[idx].isalpha():
+        tape_len += 1
+      else:
+        tape_len *= int(s[idx])
+      idx += 1
+        
+    # step through substring in reverse
+    for j in range(i-1, -1, -1):
+      c = s[j]
+      if char.isalpha():
+        if k == 0 or k == tape_len:
+          return c
+      else:
+        tape_len //= int(c)
+        k %= tape_len
+
+    # this line should never run
+    return ''
+
