c85d5078fd28fac067d1334134b8d013cb5593a0
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-06-20 23:22:14 +0900

    Start problem 1552

---
+++ Problems/1552. (M) Magnetic Force Between Two Balls/Python3/README.md
@@ -0,0 +1,9 @@
+## 1552. (M) Magnetic Force Between Two Balls
+
+### `solution.py`
+  
+
+#### Conclusion
+\<Content\>  
+  
+
---
+++ Problems/1552. (M) Magnetic Force Between Two Balls/Python3/solution.py
@@ -0,0 +1,29 @@
+class Solution:
+  def maxDistance(self, position: List[int], m: int) -> int:
+    # binary search on sorted list using greedy algorithm
+
+    n = len(position)
+    position.sort()
+    # set initial search space
+    l, h = 1, max(position) // (m-1)
+    ret = 0
+    while l <= h:
+      mid = l + (h - l) // 2
+      # validate midpoint by greedily placing ball in buckets
+      placed = 1
+      prev = position[0]
+      idx = 1
+      while idx < n:
+        if position[idx] - prev >= mid:
+          prev = position[idx]
+          placed += 1
+        idx += 1
+      # if midpoint valid, a larger value may also be valid
+      if placed >= m:
+        l = mid + 1
+        ret = mid
+      else:
+        h = mid - 1
+
+    return ret
+
