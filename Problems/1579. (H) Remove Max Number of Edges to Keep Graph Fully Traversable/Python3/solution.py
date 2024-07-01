25650bcc6eba353187654df975c14aa06ef568ce
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-07-01 17:40:07 +0900

    Make a few fixes to the write-up and slightly optimize the solution

--- Problems/1579. (H) Remove Max Number of Edges to Keep Graph Fully Traversable/Python3/solution.py
+++ Problems/1579. (H) Remove Max Number of Edges to Keep Graph Fully Traversable/Python3/solution.py
@@ -1,54 +1,55 @@
 class Solution:
+  class UnionFind:
+    def __init__(self, size):
+      self._components = size
+      self._p = [i for i in range(size)]
+    
+    def ufind(self, a):
+      while a != self._p[a]:
+        a = self._p[a]
+      return a
+    
+    def uunion(self, a, b):
+      pa, pb = self.ufind(a), self.ufind(b)
+      if pa == pb:
+        return False
+      # reversing inequality gives TLE? will need to examine this later
+      if pa > pb:
+        self._p[pb] = pa
+      else:
+        self._p[pa] = pb
+      self._components -= 1
+      return True
+    
+    def ucomponents(self):
+      return self._components
+
   def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
     # union find
 
-    alice = self.UnionFind(n)
-    bob = self.UnionFind(n)
-    req_edges = 0
-    idx = 0
+    alice, bob = self.UnionFind(n), self.UnionFind(n)
+    # sort edges by type
     edges.sort(reverse=True)
-    while idx < len(edges) and edges[idx][0] == 3:
-      a_e = alice.uunion(edges[idx][1], edges[idx][2])
-      b_e = bob.uunion(edges[idx][1], edges[idx][2])
-      if a_e or b_e:
-        req_edges += 1
+    idx = 0
+    edge_count = 0
+    # for each edge, only add it if it would reduce the number of components
+    while idx < len(edges):
+      a, b = edges[idx][1]-1, edges[idx][2]-1
+      match edges[idx][0]:
+        # add type 3 edges first
+        case 3:
+          test = alice.uunion(a, b) and bob.uunion(a, b)
+        # then the other 2 types
+        case 2:
+          test = bob.uunion(a, b)
+        case 1:
+          test = alice.uunion(a, b)
+      if test:
+        edge_count += 1
+        # return early whenever graph is traversable by both
+        if alice.ucomponents() == 1 and bob.ucomponents() == 1:
+          return len(edges) - edge_count
       idx += 1
-    while idx < len(edges) and edges[idx][0] == 2:
-      if bob.uunion(edges[idx][1], edges[idx][2]):
-        req_edges += 1
-      idx += 1
-    while idx < len(edges) and edges[idx][0] == 1:
-      if alice.uunion(edges[idx][1], edges[idx][2]):
-        req_edges += 1
-      idx += 1
-    if alice.components == 1 and bob.components == 1:
-      return len(edges) - req_edges
-    return -1
-  
-  class UnionFind:
-    def __init__(self, n = 1):
-      self._parents = [i for i in range(n+1)]
-      self.components = n
-      self._n = n
-
-    def uunion(self, a, b):
-      p_a, p_b = self._ufind(a), self._ufind(b)
-      if not p_a or not p_b or p_a == p_b:
-        return False
-      if p_a > p_b:
-        self._parents[p_b] = p_a
-      else:
-        self._parents[p_a] = p_b
-      self.components -= 1
-      return True
     
-    def _ufind(self, a):
-      if a > self._n:
-        return None
-      prev = a
-      cur = self._parents[a]
-      while cur != prev:
-        prev = cur
-        cur = self._parents[cur]
-      return cur
+    return -1
 
