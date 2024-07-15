102717f4ef5478faa12d2d2e33195ebf918f1e91
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-07-15 17:06:33 +0900

    Fix solution implementation

--- Problems/1717. (M) Maximum Score From Removing Substrings/Python3/solution.py
+++ Problems/1717. (M) Maximum Score From Removing Substrings/Python3/solution.py
@@ -4,24 +4,24 @@ class Solution:
 
     self.s = s
     # determine higher scoring substring
-    a = "ab" if x > y else "ba"
-    b = "ba" if x == "ab" else "ab"
+    a = ("ab", x) if x > y else ("ba", y)
+    b = ("ba", y) if a[0] == "ab" else ("ab", x)
 
-    def remove_ss(ss, s_ss):
+    def remove_ss(ss):
       # returns score by greedily removing ss from s where ss is worth s_ss points
       stack = []
       for c in self.s:
-        if stack and stack[-1] + c == ss:
+        if stack and stack[-1] + c == ss[0]:
           stack.pop()
         else:
           stack.append(c)
       
       # compute score from this run
-      ret = (len(self.s) - len(stack)) // 2 * s_ss
+      ret = (len(self.s) - len(stack)) // 2 * ss[1]
       # update s
       self.s = ''.join(stack)
 
       return ret
 
-    return remove_ss(a, max(x, y)) + remove_ss(b, min(x, y))
+    return remove_ss(a) + remove_ss(b)
 
