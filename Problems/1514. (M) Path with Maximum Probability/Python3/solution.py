a85217096040783fe8bde065532efc1591147441
SalinatedCoffee <74612242+SalinatedCoffee@users.noreply.github.com>
2024-08-27 16:00:48 +0900

    Slightly refactor implementation

--- Problems/1514. (M) Path with Maximum Probability/Python3/solution.py
+++ Problems/1514. (M) Path with Maximum Probability/Python3/solution.py
@@ -1,26 +1,24 @@
 class Solution:
   def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
-    # dijkstra
+    # dijkstra's algorithm
 
     adj = [[] for _ in range(n)]
-    for i in range(len(edges)):
-      u, v = edges[i]
-      adj[u].append((v, succProb[i]))
-      adj[v].append((u, succProb[i]))
+    for (u, v), p in zip(edges, succProb):
+      adj[u].append((v, p))
+      adj[v].append((u, p)) 
 
     probs = [0] * n
     probs[start] = 1
-    # hack to make min heap a max heap
+    # Python only supports min heaps, so negate values to use it as a max heap
     nodes = [(-1, start)]
     while nodes:
       p, cur = heappop(nodes)
       if cur == end:
         return -p
       for nxt, nxt_p in adj[cur]:
-        new_p = -p * nxt_p
-        if new_p > probs[nxt]:
-          probs[nxt] = new_p
-          heappush(nodes, (-probs[nxt], nxt))
+        if -p * nxt_p > probs[nxt]:
+          probs[nxt] = -p * nxt_p
+          heappush(nodes, (-p * nxt_p, nxt))
 
     return 0
 
